231d07ec19971cb848e362f17399d4c40542dcb7
motaz <motaz.dn.ali@gmail.com>
2021-11-27 12:32:57 +0300

    This commit for upload the project to the github

---
+++ src/component/Recipe.js
@@ -0,0 +1,21 @@
+import React from "react";
+import style from "./recipe.module.css"
+
+
+
+const Recipe = ({tittle, calories, image, ingredients}) =>{
+    return(
+        <div className={style.recipe}>
+          <h1>{tittle}</h1>
+          <ol>
+              {ingredients.map(ingredient =>
+              <li key={ingredients.text}>{ingredient.text}</li>
+              )}
+          </ol>
+          <p>This food has ({calories}) calories</p>
+          <img className={style.image} src={image} alt=""/>
+        </div>
+    )
+}
+
+export default Recipe;
